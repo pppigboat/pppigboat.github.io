c95c9328f1b0e89ba689656308f1255bb883beac
pppigboat <zhangquan03@gmail.com>
2014-05-19 16:53:45 +0800

    Site updated: 2014-05-19 16:53:45

--- js/twitter.js
+++ js/twitter.js
@@ -1,12 +1,12 @@
 (function($){
-  $.getScript(moment_js_path, function(){
+  $.getScript('/js/moment.min.js', function(){
     var url = 'https://api.twitter.com/1/statuses/user_timeline/' + twitter_stream[0] + '.json?count=' + twitter_stream[1] + '&exclude_replies=' + (twitter_stream[2] ? 0 : 1) + '&trim_user=true&callback=?';
 
     var linkify = function(text){
       text = text
         .replace(/(https?:\/\/)([\w\-:;?&=+.%#\/]+)/gi, '<a href="$1$2">$2</a>')
-        .replace(/(^|\W)@(\w+)/g, '$1<a href="//twitter.com/$2">@$2</a>')
-        .replace(/(^|\W)#(\w+)/g, '$1<a href="//search.twitter.com/search?q=%23$2">#$2</a>');
+        .replace(/(^|\W)@(\w+)/g, '$1<a href="http://twitter.com/$2">@$2</a>')
+        .replace(/(^|\W)#(\w+)/g, '$1<a href="http://search.twitter.com/search?q=%23$2">#$2</a>');
 
       return text;
     };
